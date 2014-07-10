dedae81adc2ba6ae35030b1a961405170fe8f771
heresandyboy <heresandyboy@gmail.com>
2014-07-10 10:10:35 +0100

    Initial GitHub seed

---
+++ www/js/app.js
@@ -0,0 +1,5 @@
+﻿angular.module('myapp', ['mobileServices']). // setting up our app to use the mobileServices service
+    config(['$httpProvider', function ($httpProvider) { // coniguring the httpProvider
+        $httpProvider.defaults.headers.common['X-ZUMO-APPLICATION'] = 'RkahlgSDnwpkXUfLBfySnWDcRDZICp27'; // add the application key
+        $httpProvider.defaults.headers.common['Content-Type'] = 'Application/json';
+    }]);
---
+++ www/js/mobile-service.js
@@ -0,0 +1,11 @@
+﻿// mobile-service.js
+'use strict';
+
+angular.module('mobileServices', ['ngResource']). // new service mobileServices depending on ngResource
+    factory('Task', function ($resource) { // declaring a MyTable resource
+        return $resource('https://andyainsworth.azure-mobile.net/tables/task/:taskId', { taskId: '@id' }, // binding to the table url
+            {
+                'update': { method: 'PATCH' } // adding an update function
+            }
+        );
+    });

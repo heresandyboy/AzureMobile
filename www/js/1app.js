1970c950fbe46a5d452df75d95912f578c704902
heresandyboy <heresandyboy@gmail.com>
2014-08-13 17:12:28 +0100

    Start to remove unused code
    Remove bits of code introduced to assist in learning ionic and angularJS

--- www/js/1app.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('myapp', ['mobileServices']). // setting up our app to use the mobileServices service
-    config(['$httpProvider', function ($httpProvider) { // coniguring the httpProvider
-        $httpProvider.defaults.headers.common['X-ZUMO-APPLICATION'] = 'RkahlgSDnwpkXUfLBfySnWDcRDZICp27'; // add the application key
-        $httpProvider.defaults.headers.common['Content-Type'] = 'Application/json';
-    }]);
